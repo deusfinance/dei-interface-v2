4cf7f1abddec3ed72f692d2f42572ca479d36ef7
Mohammad Ehsan Kaji <mr.ehsan.kaji@gmail.com>
2022-10-22 14:36:40 +0330

    feat: add usdc reserve 3

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -75,6 +75,10 @@ export const USDCReserves2: AddressMap = {
   [SupportedChainId.FANTOM]: '0xfd74e924dc96c72ba52439e28ce780908a630d13',
 }
 
+export const USDCReserves3: AddressMap = {
+  [SupportedChainId.FANTOM]: '0x37a7a2a5FCB0DF6B8138fec7730825E92f9D8207',
+}
+
 /* =====================================
                 DAPP CONTRACTS ADDRESS
 ===================================== */
