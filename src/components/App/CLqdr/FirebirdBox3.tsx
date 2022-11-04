1fd9a4ea8d1695aea797456c0970eabc16ee98ea
Mohammad Ehsan Kaji <mr.ehsan.kaji@gmail.com>
2022-11-04 18:25:05 +0330

    feat: add firebird boxes

--- src/components/App/CLqdr/FirebirdBox3.tsx
+++ src/components/App/CLqdr/FirebirdBox3.tsx
@@ -5,66 +5,18 @@ import ImageWithFallback from 'components/ImageWithFallback'
 import FIREBIRD_ICON from '/public/static/images/pages/clqdr/ic_firebird.svg'
 import { ExternalLink } from 'components/Link'
 import { RowEnd } from 'components/Row'
-
-const Wrapper = styled.div`
-  margin-top: 16px;
-  border-radius: 12px;
-  width: clamp(250px, 90%, 500px);
-  background: ${({ theme }) => theme.bg3};
-  padding: 12px;
-  height: 88px;
-  position: relative;
-  overflow: hidden;
-
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-    // height: 50px;
-  `}
-`
+import { Wrapper, BuyButtonWrapper, BuyButton } from '.'
 
 const BuyWrapper = styled(RowEnd)`
   margin-top: 2px;
 `
 
-const BuyButtonWrapper = styled.div`
-  padding: 2px;
+const ButtonWrapper = styled(BuyButtonWrapper)`
   width: 165px;
-  height: 56px;
-  background: linear-gradient(90deg, #f34038 0%, #ffb396 52.08%, #f78c2a 100%);
-  border-radius: 12px;
 
   ${({ theme }) => theme.mediaWidth.upToExtraSmall`
     height: 36px;
     width:100px;
-  `}
-`
-
-const BuyButton = styled.div`
-  width: 100%;
-  height: 100%;
-
-  background: linear-gradient(90deg, #f78c2a 0%, #f34038 100%),
-    linear-gradient(90deg, #f34038 0%, #ffb396 52.08%, #f78c2a 100%);
-
-  border-radius: 12px;
-
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 19px;
-
-  text-align: center;
-  outline: none;
-  text-decoration: none;
-  display: flex;
-  justify-content: center;
-  flex-wrap: nowrap;
-  align-items: center;
-
-  color: ${({ theme }) => theme.text1};
-
-  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
-    font-size: 12px;
   `}
 `
 
@@ -99,7 +51,7 @@ const FireBirdWrap = styled.div`
   white-space: nowrap;
 `
 
-export default function FireBirdBox() {
+export default function FirebirdBox3() {
   return (
     <Wrapper>
       <Icon>
@@ -111,11 +63,11 @@ export default function FireBirdBox() {
           <FirebirdText>Buy it on Firebird Finance</FirebirdText>
         </div>
         <BuyWrapper>
-          <BuyButtonWrapper>
-            <ExternalLink href={'https://beets.fi/swap'} style={{ textDecoration: 'none' }}>
+          <ButtonWrapper>
+            <ExternalLink href={'https://app.firebird.finance/swap'} style={{ textDecoration: 'none' }}>
               <BuyButton>Buy LQDR</BuyButton>
             </ExternalLink>
-          </BuyButtonWrapper>
+          </ButtonWrapper>
         </BuyWrapper>
       </FireBirdWrap>
     </Wrapper>
