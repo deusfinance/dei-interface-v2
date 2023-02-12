6f7a5a216c22584d365902b80569ffdcb9fc1e6e
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2023-02-12 22:21:57 +0330

    feat: add my stake dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { Container } from 'components/App/StableCoin'
@@ -7,6 +7,8 @@ import Stats from 'components/App/Dashboard/Stats'
 import MigrationNav from 'components/App/Dashboard/MigrationNav'
 import Account from 'components/App/Dashboard/Account'
 import { useWeb3NavbarOption } from 'state/web3navbar/hooks'
+import Staking from 'components/App/Dashboard/Staking'
+import { Stakings } from 'constants/stakingPools'
 
 const Wrapper = styled(RowCenter)`
   max-width: 1300px;
@@ -32,12 +34,15 @@ const Wrapper = styled(RowCenter)`
 
 export default function Dashboard() {
   useWeb3NavbarOption({ network: true, wallet: true, reward: true })
+  const threshold = 2
+  const [results] = useState(() => Stakings.slice(0, Stakings.length > 2 ? threshold : Stakings.length))
 
   return (
     <Container>
       <Wrapper>
         <MigrationNav />
         <Account />
+        {results.length !== 0 && <Staking stakings={results} />}
         <Stats />
       </Wrapper>
     </Container>
