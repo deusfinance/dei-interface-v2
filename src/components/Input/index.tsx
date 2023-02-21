f54c48cfc5b05099c04dca02141f943e54ceb5e6
Shadow <87962799+shadowcrypto1@users.noreply.github.com>
2023-02-21 14:59:33 +0530

    fix input box crash error

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -61,6 +61,7 @@ export const NumericalInput = ({
       onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
         // replace commas with periods
         enforcer(event.target.value.replace(/,/g, '.'))
+        enforcer(event.target.value.replace(/^\./g, '0.'))
       }}
       // universal input options
       inputMode="decimal"
