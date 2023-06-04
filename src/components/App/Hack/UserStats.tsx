1a8661bf728fd25332ecdad2055f550795ef88fe
Mohammad Ali <maliabr75@gmail.com>
2023-06-05 01:30:39 +0330

    feat: add incident data

---
+++ src/components/App/Hack/UserStats.tsx
@@ -0,0 +1,143 @@
+import styled from 'styled-components'
+import Column from 'components/Column'
+import { RowStart } from 'components/Row'
+import { BN_TEN, formatBalance, toBN } from 'utils/numbers'
+
+const MainTitle = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 2px;
+`
+const Title = styled.div`
+  font-size: 16px;
+  color: ${({ theme }) => theme.text2};
+`
+const Amount = styled.div`
+  font-size: 16px;
+  color: ${({ theme }) => theme.yellow1};
+`
+function has(amount: string): boolean {
+  return toBN(amount).abs().gt(0)
+}
+
+function toAmount(amount: string, decimals?: number): string {
+  return formatBalance(
+    toBN(amount)
+      .abs()
+      .div(BN_TEN.pow(decimals || 18))
+  )
+}
+
+function getDecimals(name: string, symbol: string) {
+  if (name.indexOf('Thena') !== -1 || name.indexOf('archly-bnb') !== -1 || name.indexOf('archly-bnb') !== -1) {
+    return 18
+  }
+  return symbol === 'USDC' || symbol === 'USDT' ? 6 : 18
+}
+
+function getInfoDynamic(data: any) {
+  return Object.keys(data)
+    .filter((k) => has(data[k]))
+    .map((v) => ({ title: v, amount: `${toAmount(data[v], getDecimals('', v))}` }))
+}
+
+function getLPInfo(pairs: any) {
+  const { al_t0, al_t1, lp_bh, lp_now, rl_t0, rl_t1, t0_bh, t0_now, t1_bh, t1_now, token0, token1, name } = pairs || {}
+  return [
+    {
+      title: 'Balance before',
+      amount: `${toAmount(lp_bh)} LP (${toAmount(t0_bh, getDecimals(name, token0))} ${token0} + ${toAmount(
+        t1_bh,
+        getDecimals(name, token1)
+      )} ${token1})`,
+    },
+    {
+      title: 'Balance now',
+      amount: `${toAmount(lp_now)} LP (${toAmount(t0_now, getDecimals(name, token0))} ${token0} + ${toAmount(
+        t1_now,
+        getDecimals(name, token1)
+      )} ${token1})`,
+    },
+    {
+      title: 'Add liquidity',
+      amount: `${toAmount(al_t0, getDecimals(name, token0))} ${token0} + ${toAmount(
+        al_t1,
+        getDecimals(name, token1)
+      )} ${token1}`,
+    },
+    {
+      title: 'Remove liquidity',
+      amount: `${toAmount(rl_t0, getDecimals(name, token0))} ${token0} + ${toAmount(
+        rl_t1,
+        getDecimals(name, token1)
+      )} ${token1}`,
+    },
+  ]
+}
+
+function Stats({
+  mainTitle,
+  info,
+}: {
+  mainTitle: string
+  info: Array<{ title: string; amount: string }>
+}): JSX.Element | null {
+  return (
+    <Column style={{ gap: '6px', width: '100%' }}>
+      <MainTitle>{mainTitle}</MainTitle>
+
+      {info.map((item, key) => (
+        <RowStart key={key} style={{ gap: '8px' }}>
+          <Title>{item.title}:</Title>
+          <Amount>{item.amount}</Amount>
+        </RowStart>
+      ))}
+    </Column>
+  )
+}
+
+export default function UserStats({ userData }: { userData: any }) {
+  const { pairs } = userData
+  const { anydei_bh, dei_bh, anydei_now, dei_now } = userData.balance
+  const { dei, deus, usdc } = userData.redeemed
+  const DeiBalance = [
+    { title: 'before', amount: toAmount(dei_bh) },
+    { title: 'now', amount: toAmount(dei_now) },
+  ]
+  const anyDeiBalance = [
+    { title: 'before', amount: toAmount(anydei_bh) },
+    { title: 'now', amount: toAmount(anydei_now) },
+  ]
+  const deiRedeemed = [
+    { title: 'DEI', amount: toAmount(dei) },
+    { title: 'DEUS', amount: toAmount(deus) },
+    { title: 'USDC', amount: toAmount(usdc, 6) },
+  ]
+  const burned = [{ title: 'DEI', amount: toAmount(userData.burned) }]
+
+  function isNoneZero(info: Array<{ title: string; amount: string }>): boolean {
+    return info.filter((item) => has(item.amount)).length > 0
+  }
+
+  return (
+    <Column style={{ gap: '35px', width: '100%' }}>
+      {isNoneZero(DeiBalance) && <Stats mainTitle="DEI balance" info={DeiBalance} />}
+      {isNoneZero(anyDeiBalance) && <Stats mainTitle="anyDEI balance" info={anyDeiBalance} />}
+      {isNoneZero(deiRedeemed) && <Stats mainTitle="DEI Redeemed" info={deiRedeemed} />}
+      {isNoneZero(burned) && <Stats mainTitle="Burned" info={burned} />}
+
+      {getInfoDynamic(userData['buy']).length > 0 && <Stats mainTitle="Buys" info={getInfoDynamic(userData['buy'])} />}
+      {getInfoDynamic(userData['sell']).length > 0 && (
+        <Stats mainTitle="Sells" info={getInfoDynamic(userData['sell'])} />
+      )}
+
+      {getInfoDynamic(userData['arbitrage']).length > 0 && (
+        <Stats mainTitle="Arbitrages" info={getInfoDynamic(userData['arbitrage'])} />
+      )}
+
+      {pairs.map((pair: any, key: number) => (
+        <Stats key={key} mainTitle={pair.name} info={getLPInfo(pair)} />
+      ))}
+    </Column>
+  )
+}
--- src/components/App/Hack/index.tsx
+++ src/components/App/Hack/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 import { Container as MainContainer } from 'components/App/StableCoin'
 import { ColumnCenter } from 'components/Column'
@@ -8,13 +8,14 @@ import { InputField } from 'components/Input'
 import { RowBetween } from 'components/Row'
 import { BaseButton } from 'components/Button'
 
-import LPsData from 'constants/files/deus-dei-lp-hack.json'
 import { AddressZero } from '@ethersproject/constants'
 import { isAddress } from 'utils/address'
 import useWeb3React from 'hooks/useWeb3'
-import { RESULT, TNotification, UserData, UserHackData } from './Types'
-import { toBN } from 'utils/numbers'
-// import { toBN } from 'utils/numbers'
+import { RESULT, UserData } from './Types'
+import UserStats from './UserStats'
+import { makeHttpRequest } from 'utils/http'
+import { ExternalLink } from 'components/Link'
+import { NavButton } from 'components/Button'
 
 const Container = styled(MainContainer)`
   min-height: 90vh;
@@ -22,24 +23,16 @@ const Container = styled(MainContainer)`
   justify-content: center;
   align-items: center;
   position: relative;
-  &:before {
-    position: absolute;
-    width: 720px;
-    height: 510px;
-    content: '';
-    background: linear-gradient(90deg, #0badf4 0%, #30efe4 53.12%, #ff9af5 99.99%);
-    opacity: 0.1;
-    filter: blur(120px);
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
+  /* background: linear-gradient(90deg, #0baef422 0%, #30efe422 53.12%, #ff9af522 99.99%); */
 `
 const FormContainer = styled(ColumnCenter)`
-  width: 720px;
-  height: 510px;
+  width: 100%;
+  max-width: 900px;
+  padding: 0 20px;
+  /* width: 720px;
+  height: 510px; */
   /* background-color: #eee; */
-  justify-content: center;
+  /* justify-content: center;
 
   ${({ theme }) => theme.mediaWidth.upToMedium`
     width: 400px;
@@ -47,14 +40,13 @@ const FormContainer = styled(ColumnCenter)`
 
   ${({ theme }) => theme.mediaWidth.upToSmall`
     width: 340px;
-  `}
+  `} */
 `
-const FormHeader = styled.h2`
-  font-weight: 500;
-  font-size: 2rem;
+const FormHeader = styled.div`
+  margin-top: 50px;
+  font-weight: 800;
+  font-size: 32px;
   color: ${({ theme }) => theme.text1};
-  margin-top: 2px;
-  font-family: 'Space Grotesk';
 `
 const WalletAddressInputContainer = styled(RowBetween)`
   margin-block: 34px;
@@ -69,9 +61,11 @@ const WalletAddressInputContainer = styled(RowBetween)`
     height: 45px;
   `}
 `
+
 const Button = styled(BaseButton)`
   height: 100%;
-  background-image: linear-gradient(90deg, #0badf4 0%, #30efe4 93.4%);
+  background-image: linear-gradient(90deg, #f34038 0%, #ffb396 52.08%, #f78c2a 100%);
+  color: #000;
   border-radius: 8px;
   width: 100px;
   font-size: 1rem;
@@ -94,38 +88,28 @@ const Input = styled(InputField)`
   `}
 `
 
-const Notification = styled.p<{ type: RESULT }>`
-  background: ${({ type }) =>
-    type === RESULT.ELIGIBLE ? 'linear-gradient(90deg, #0BADF4 0%, #30EFE4 93.4%)' : '#F367E2'};
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  display: inline-block;
-`
-const NotificationContainer = styled.p`
-  text-align: center;
-  font-size: 2rem;
-  font-weight: medium;
-
-  ${({ theme }) => theme.mediaWidth.upToMedium`
-    font-size: 1rem;
-    margin-top: -15px;
-  `}
-`
-
 export default function Hack() {
   const { account } = useWeb3React()
   const [result, setResult] = useState<RESULT | undefined>(undefined)
   const [walletAddress, setWalletAddress] = useState<string>('')
   const [data, setData] = useState<UserData[]>([])
+  const [userData, setUserData] = useState<any>(null)
 
-  const eligibleContent: TNotification = { content: 'Your wallet is in snapshot', emoji: ' 🥳' }
-  const notEligibleContent: TNotification = { content: 'Your wallet is not in snapshot', emoji: ' 😖' }
+  const findUserLPData = useCallback(async () => {
+    try {
+      const res = makeHttpRequest(`https://info.deus.finance/info/dei/userData/${walletAddress}`)
+      return res
+    } catch (error) {
+      return null
+    }
+  }, [walletAddress])
 
   useEffect(() => {
-    if (account) {
-      setWalletAddress(account)
-      findUserLPData(account)
-    }
+    findUserLPData()
+  }, [findUserLPData])
+
+  useEffect(() => {
+    if (account) setWalletAddress(account)
   }, [account])
 
   useEffect(() => {
@@ -134,38 +118,22 @@ export default function Hack() {
     }
   }, [walletAddress])
 
-  function findUserLPData(walletAddress: string): void {
-    const AllData: UserHackData[] = LPsData as unknown as UserHackData[]
-    const data: UserData[] = []
-    for (let i = 0; i < AllData.length; i++) {
-      const lpData = AllData[i]
-
-      if (lpData.user.toLowerCase() === walletAddress.toLowerCase()) {
-        setResult(RESULT.ELIGIBLE)
-        const userData: UserHackData = lpData
-
-        for (const key in userData) {
-          if (Object.prototype.hasOwnProperty.call(userData, key)) {
-            // console.log(key, userData[key as keyof UserHackData])
-            const rowValue = userData[key as keyof UserHackData]
-            if (!toBN(rowValue).isEqualTo(0) && key !== 'name' && key !== 'user' && key !== 'is_contract')
-              data.push({ key, value: rowValue })
-          }
-        }
-
-        console.log(data)
-        setData(data)
-        return
-      } else {
-        setResult(RESULT.NOT_ELIGIBLE)
-      }
-    }
+  const handleCheck = async () => {
+    const rest = await findUserLPData()
+    setUserData(rest)
   }
+
+  // function findUserLPData(walletAddress: string): void {
+  //   const AllData = LPsData as unknown as { [key: string]: any }
+  //   const userData = AllData[walletAddress.toLowerCase()] ?? null
+  //   setUserData(userData)
+  //   return userData
+  // }
 
   return (
     <Container>
       <FormContainer>
-        <FormHeader>Hack</FormHeader>
+        <FormHeader>Dei snapshot 05.05.2023</FormHeader>
         <WalletAddressInputContainer>
           <Input
             value={walletAddress}
@@ -174,28 +142,20 @@ export default function Hack() {
           />
           <Button
             onClick={() => {
-              findUserLPData(walletAddress)
+              handleCheck()
             }}
           >
             Check
           </Button>
         </WalletAddressInputContainer>
-        {result === RESULT.ELIGIBLE ? (
-          <div>
-            <NotificationContainer>
-              <Notification type={RESULT.NOT_ELIGIBLE}>{notEligibleContent.content}</Notification>
-              {notEligibleContent.emoji}
-            </NotificationContainer>
-            {data.map((d: UserData, index) => (
-              <div key={index}>{`${d.key}: ${d.value}`}</div>
-            ))}
-          </div>
-        ) : result === RESULT.NOT_ELIGIBLE ? (
-          <NotificationContainer>
-            <Notification type={RESULT.NOT_ELIGIBLE}>{notEligibleContent.content}</Notification>
-            {notEligibleContent.emoji}
-          </NotificationContainer>
-        ) : null}
+
+        {userData && <UserStats userData={userData} />}
+
+        {userData && (
+          <ExternalLink style={{ marginTop: '60px' }} href="https://7eoku1wmhjg.typeform.com/dei-incident">
+            <NavButton style={{ width: '200px', padding: '5px 10px' }}>Report Issues</NavButton>
+          </ExternalLink>
+        )}
       </FormContainer>
     </Container>
   )
